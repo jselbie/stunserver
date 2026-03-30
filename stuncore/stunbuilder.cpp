bddac12707d797001e5bad3c5e7657d18e672b4c
sherla <sherlacheung@vtech.ca>
2026-03-29 20:33:51 -0700

    Fix: * rfc3489 does not use magic cookie in transaction id * the last 4 bytes of transaction id were not populated

--- stuncore/stunbuilder.cpp
+++ stuncore/stunbuilder.cpp
@@ -93,7 +93,6 @@ HRESULT CStunMessageBuilder::AddTransactionId(const StunTransactionId& transid)
 HRESULT CStunMessageBuilder::AddRandomTransactionId(StunTransactionId* pTransId)
 {
     StunTransactionId transid;
-    uint32_t stun_cookie_nbo = htonl(STUN_COOKIE);
 
     uint32_t entropy=0;
 
@@ -135,10 +134,18 @@ HRESULT CStunMessageBuilder::AddRandomTransactionId(StunTransactionId* pTransId)
     srand(entropy);
 
 
-    // the first four bytes of the transaction id is always the magic cookie
-    // followed by 12 bytes of the real transaction id
-    memcpy(transid.id, &stun_cookie_nbo, sizeof(stun_cookie_nbo));
-    for (int x = 4; x < (STUN_TRANSACTION_ID_LENGTH-4); x++)
+    // rfc3489 (legacy mode) does not use magic cookie
+    int x = 0;
+    if (!_fLegacyMode)
+    {
+        // the first four bytes of the transaction id is always the magic cookie
+        // followed by 12 bytes of the real transaction id
+        uint32_t stun_cookie_nbo = htonl(STUN_COOKIE);
+        memcpy(transid.id, &stun_cookie_nbo, sizeof(stun_cookie_nbo));
+        x = 4;
+    }
+
+    for (; x < STUN_TRANSACTION_ID_LENGTH; x++)
     {
         transid.id[x] = (uint8_t)(rand() % 256);
     }
